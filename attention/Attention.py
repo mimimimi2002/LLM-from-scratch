5798773eb0401f6235dc1af6ee45e5082727e470
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-07-25 14:16:28 +0900

    fix: attention typo

--- attention/Attention.py
+++ attention/Attention.py
@@ -3,10 +3,9 @@ import torch
 class SelfAttention(nn.Module):
   def __init__(self, d_in, d_out, qkv_bias=False):
     super().__init__()
-    self.d_out = d_out
-    self.W_query = nn.Linear(d_in, d_out, qkv_bias)
-    self.W_key = nn.Linear(d_in, d_out, qkv_bias)
-    self.W_value = nn.Linear(d_in, d_out, qkv_bias)
+    self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+    self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+    self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
 
   def forward(self, x):
     keys = self.W_key(x)
@@ -14,14 +13,9 @@ class SelfAttention(nn.Module):
     values = self.W_value(x)
 
     attn_scores = queries @ keys.T
+    attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
 
-    # normalization
-    atten_weights = torch.softmax(
-      attn_scores / keys.shape[-1]**0.5, dim=-1
-    )
-
-    # multiply value
-    context_vec = atten_weights @ values
+    context_vec = attn_weights @ values
     return context_vec
 
 if __name__ == "__main__":
