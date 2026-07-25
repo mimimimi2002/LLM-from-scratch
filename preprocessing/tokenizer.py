0c28f334191cb85ad2cd7ab494491d89e2143f0a
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-07-25 13:52:30 +0900

    fix: tokenizer add punctuation

--- preprocessing/tokenizer.py
+++ preprocessing/tokenizer.py
@@ -4,22 +4,22 @@ EOT = "<|endoftext|>"
 
 class SimpleTokenizer:
   def __init__(self, vocab: list[str]):
-    self.word_to_token = {word:token for token, word in enumerate(vocab)}
-    self.token_to_word = {token:word for token, word in enumerate(vocab)}
+    self.word_to_token = {word: token for token, word in enumerate(vocab)}
+    self.token_to_word = {token: word for token, word in enumerate(vocab)}
 
-  @classmethod
-  def create_vocabulary(self, file_name):
+  @staticmethod
+  def create_vocabulary(file_name):
     try:
       with open(file_name, "r") as f:
         raw_text = f.read()
     except OSError as e:
       raise OSError(f"cannot read vocabulary from file: {file_name}") from e
     except UnicodeDecodeError as e:
-      raise ValueError(f"cannot read {file_name} as UTF-8")
+      raise ValueError(f"cannot read {file_name} as UTF-8") from e
 
     preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
     preprocessed = [s.strip() for s in preprocessed if s.strip()]
-    vocab =  sorted(set(preprocessed))
+    vocab = sorted(set(preprocessed))
     vocab.extend([EOT, UNK])
     return vocab
 
@@ -28,11 +28,12 @@ class SimpleTokenizer:
     preprocessed = [s.strip() for s in preprocessed if s.strip()]
     preprocessed = [s if s in self.word_to_token else UNK for s in preprocessed]
     # turn to tokenIDs
-    return [self.word_to_token[s] if s in self.word_to_token else UNK for s in preprocessed]
+    return [self.word_to_token[s] for s in preprocessed]
 
   def decode(self, ids):
-    text = " ".join([self.token_to_word[id] if id in self.token_to_word else "" for id in ids])
-    text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+    text = " ".join([self.token_to_word[id] for id in ids])
+    # Replace spaces before the specified punctuations
+    text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
     return text
 
 if __name__ == "__main__":
