557cf16c4fc9dc77e77b29d0fcf88afbcf2367f8
mimimimi2002 <mimimimi2002.ma@gmail.com>
2026-07-25 13:51:08 +0900

    fix; stride in dataset loader

--- preprocessing/dataloader.py
+++ preprocessing/dataloader.py
@@ -3,15 +3,19 @@ from torch.utils.data import Dataset, DataLoader
 import tiktoken
 
 class GPTDataset(Dataset):
-  def __init__(self, txt, tokenizer, max_length, stride = 1):
+  def __init__(self, txt, tokenizer, max_length, stride):
     self.input_ids = []
     self.target_ids = []
 
-    token_ids = tokenizer.encode(txt)
+    # Tokenize the entire text
+    token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
 
+    # Use a sliding window to chunk the book into overlapping sequences of max_length
     for i in range(0, len(token_ids) - max_length, stride):
-      self.input_ids.append(token_ids[i: i + max_length])
-      self.target_ids.append(token_ids[i + stride: i + max_length + stride])
+      input_chunk = token_ids[i: i + max_length]
+      target_chunk = token_ids[i + 1: i + max_length + 1]
+      self.input_ids.append(torch.tensor(input_chunk))
+      self.target_ids.append(torch.tensor(target_chunk))
 
   def __len__(self):
     return len(self.input_ids)
@@ -23,10 +27,13 @@ class GPTDataset(Dataset):
 def create_dataloader(txt, batch_size=4, max_length=256,
   stride=128, shuffle=True, drop_last=True, num_workers=0):
 
-  # vocab = SimpleTokenizer.create_vocabulary("./book.txt")
-  # tokenizer = SimpleTokenizer(vocab)
+  # Initialize the tokenizer
   tokenizer = tiktoken.get_encoding("gpt2")
+
+  # Create dataset
   dataset = GPTDataset(txt, tokenizer, max_length, stride)
+
+  # Create dataloader
   dataloader = DataLoader(
     dataset,
     batch_size=batch_size,
